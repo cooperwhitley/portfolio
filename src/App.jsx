c95c6f5bf4899273ba5c119b2e5f60524a655568
cooperwhitley <whitleycooperr@gmail.com>
2023-11-06 10:53:33 -0600

    create component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/shared/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Cooper portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
