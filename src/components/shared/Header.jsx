f0ee93569aca08e487d17c9e3f2d2d87292e0e49
cooperwhitley <whitleycooperr@gmail.com>
2023-11-06 10:53:41 -0600

    create component

---
+++ src/components/shared/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <div>
+            <a href="https://github.com/cooperwhitley">github</a>
+            <a href="https://www.linkedin.com/in/cooperwhitley/">linkedin</a>
+        </div>
+    )
+}
