b46bf381200a0ca243408080791c68a91e68119d
cooperwhitley <whitleycooperr@gmail.com>
2023-11-08 11:15:51 -0600

    restructure for bootstrap

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,8 +1,18 @@
+import { Container, Nav, Navbar } from 'react-bootstrap'
+
 export default function Header() {
     return (
-        <div>
-            <a href="https://github.com/cooperwhitley">github</a>
-            <a href="https://www.linkedin.com/in/cooperwhitley/">linkedin</a>
-        </div>
+        <Navbar expand='lg' className='bg-body-tertiary'>
+            <Container>
+                <Navbar.Brand>Cooper Whitley</Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className="me-auto">
+                        <Nav.Link href="https://github.com/cooperwhitley">github</Nav.Link>
+                        <Nav.Link href="https://www.linkedin.com/in/cooperwhitley/">linkedin</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
