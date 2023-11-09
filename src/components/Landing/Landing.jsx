53b9d2f20de09ccaec3539c7dc53586a242d33d2
cooperwhitley <whitleycooperr@gmail.com>
2023-11-09 11:16:46 -0600

    placeholder landing component

---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,8 @@
+export default function Landing() {
+    return(
+        <>
+            <h1>Cooper Whitley</h1>
+            <p>landing info here blah blah blah whatever</p>
+        </>
+    )
+}
