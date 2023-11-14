ac33d5f0765fb0a96728fa2e01986b3bc5a9f89d
cooperwhitley <whitleycooperr@gmail.com>
2023-11-13 18:52:41 -0600

    add styling to link buttons

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -2,6 +2,10 @@ import { Container, Nav, Navbar } from 'react-bootstrap'
 let Cooper = require('../../assets/cooper.png')
 
 export default function Header() {
+    let linkButtonStyle = {
+        border: 'none',
+        background: 'none'
+    }
     return (
         <Navbar expand='lg' className='bg-body-tertiary' style={{position: 'fixed', width: '100vw', zIndex: '1', height: '5vmin'}}>
             <Container>
@@ -15,8 +19,9 @@ export default function Header() {
                 </Navbar.Brand>
                 <Navbar.Brand style={{fontWeight: 'bold'}}>cooper whitley - <span style={{fontWeight: '200', fontStyle: 'italic'}}>software engineer</span></Navbar.Brand>
                 <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                <Navbar.Collapse id='basic-navbar-nav'>
-                    <Nav className="me-auto">
+                <Navbar.Collapse id='basic-navbar-nav' className='justify-content-end'>
+                    <Nav className="me-auto" >
+                        <Nav.Link><button style={linkButtonStyle}>contact me</button></Nav.Link>
                         <Nav.Link href="https://github.com/cooperwhitley" target="_blank">github</Nav.Link>
                         <Nav.Link href="https://www.linkedin.com/in/cooperwhitley/" target="_blank">linkedin</Nav.Link>
                     </Nav>
